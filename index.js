a2552f3d250931b5a5ae224d877da73a32a6b436
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2022-11-04 19:19:22 -0300

    Utilizando método nativo Assert

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+import assert from 'node:assert/strict';
+
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      assert.strictEqual(valor, esperado);
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoTeste) => {
+  try {
+    funcaoTeste();
+
     console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
+  } catch {
+    console.error(`${titulo} não passou!!!`);
   }
 };
 
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+teste('somaHorasExtras', () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
 
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste('calculaDesconto', () => {
+  const esperado = 2300;
+  const retornado = calculaDescontos(2500, 200);
+
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
