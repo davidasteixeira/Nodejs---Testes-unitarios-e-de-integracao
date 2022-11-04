9096c34d968d5847f5ecb95ea6d42ddd7476d53d
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2022-11-04 19:55:16 -0300

    Adicionando pacote Jest ao projeto

--- index.js
+++ index.js
@@ -4,35 +4,7 @@ const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtra
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const verifiqueSe = (valor) => {
-  const assercoes = {
-    ehExatamenteIgualA(esperado) {
-      assert.strictEqual(valor, esperado);
-    },
-  };
-  return assercoes;
+export {
+  somaHorasExtras,
+  calculaDescontos,
 };
-
-const teste = (titulo, funcaoTeste) => {
-  try {
-    funcaoTeste();
-
-    console.log(`${titulo} passou`);
-  } catch {
-    console.error(`${titulo} não passou!!!`);
-  }
-};
-
-teste('somaHorasExtras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
-
-  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
-
-teste('calculaDesconto', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
-
-  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
